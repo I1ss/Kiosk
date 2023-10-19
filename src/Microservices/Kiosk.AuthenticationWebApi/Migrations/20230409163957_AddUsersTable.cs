e6cb41f5a6337e7708e20e493c93b9d1e0da69f7
I1ss <lebedewgeorg@yandex.ru>
2023-10-19 17:12:55 +0700

    Dev-ChangedMigration - Изменена миграция (добавлены пользователи под каждый тип роли).

--- src/Microservices/Kiosk.AuthenticationWebApi/Migrations/20230409163957_AddUsersTable.cs
+++ src/Microservices/Kiosk.AuthenticationWebApi/Migrations/20230409163957_AddUsersTable.cs
@@ -26,6 +26,9 @@ namespace Kiosk.AuthenticationWebApi.Migrations
                     table.PrimaryKey("PK_Users", x => x.User_Id);
                 });
 
+            migrationBuilder.Sql("INSERT INTO \"Users\" (\"User_Name\", \"User_Password\", \"User_Role\") VALUES ('Client', '0c77fe09ab336a3e57cc2b6804a9bac65e19a27782b5a9c815d585e894c3b7f2', 0)");
+            migrationBuilder.Sql("INSERT INTO \"Users\" (\"User_Name\", \"User_Password\", \"User_Role\") VALUES ('Courier', 'bbc5df524d41b1e3bf11709dc12e69563b07d87e3706ededd847ad6a5e59a8f0', 1)");
+            migrationBuilder.Sql("INSERT INTO \"Users\" (\"User_Name\", \"User_Password\", \"User_Role\") VALUES ('Seller', '01498fa31d5a344a6a66c69dc5671c8d0f86953c2268c1c9274b259add7bae4b', 2)");
             migrationBuilder.Sql("INSERT INTO \"Users\" (\"User_Name\", \"User_Password\", \"User_Role\") VALUES ('Admin', 'c1c224b03cd9bc7b6a86d77f5dace40191766c485cd55dc48caf9ac873335d6f', 3)");
         }
 
